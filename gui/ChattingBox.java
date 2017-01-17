5271b867e61b92eb35bbdd32696cef136f592b17
kqwyf <kqwyfhit@163.com>
2017-01-18 00:32:11 +0800

    聊天框

--- gui/ChattingBox.java
+++ gui/ChattingBox.java
@@ -1,9 +1,17 @@
 //package gui;
-import javax.swing.*;
+import javax.swing.JPanel;
+import javax.swing.JTextPane;
+import javax.swing.JFrame;
+import java.awt.Dimension;
+import javax.swing.text.html.HTMLEditorKit;
+import javax.swing.text.html.HTMLDocument;
+import javax.swing.text.html.HTML.Tag;
 
 public class ChattingBox extends JPanel
 {
-	private JTextPane myPane;
+	private JTextPane myPane=new JTextPane();
+	private HTMLEditorKit he=new HTMLEditorKit();
+	private HTMLDocument hd=(HTMLDocument) he.createDefaultDocument();
 
 	public static final void main(String[] args)
 	{
@@ -12,12 +20,28 @@ public class ChattingBox extends JPanel
 		f.getContentPane().add(c);
 		f.pack();
 		f.setVisible(true);
+		c.pushMessage("");
+		c.pushMessage("");
+		c.pushMessage("");
 	}
 
 	ChattingBox()
 	{
-		myPane=new JTextPane();
+		myPane.setPreferredSize(new Dimension(100,200));
+		myPane.setEditorKit(he);
+		myPane.setContentType("text/html");
+		myPane.setDocument(hd);
 		myPane.setVisible(true);
 		this.add(myPane);
+	}
+
+	public void pushMessage(String message)
+	{
+		try{
+			he.insertHTML(hd,hd.getLength(),
+				"<table border=\"0\"><tr><td>000</td><td>123</td><td>456</td></tr></table>",
+				0,0,Tag.TABLE);
+			he.insertHTML(hd,hd.getLength(),"<br>",0,0,Tag.BR);
+		}catch(Exception e){}
 	}
 }
