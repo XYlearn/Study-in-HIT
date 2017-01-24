31c2d3cb47e31f9c731a08bc480f521aa582ac78
kqwyf <kqwyfhit@163.com>
2017-01-24 23:13:21 +0800

    聊天框相关

--- gui/ChattingBox.java
+++ gui/ChattingBox.java
@@ -9,9 +9,10 @@ import javax.swing.text.html.HTML.Tag;
 
 public class ChattingBox extends JPanel
 {
-	private JTextPane myPane=new JTextPane();
+	public JTextPane myPane=new JTextPane();
 	private HTMLEditorKit he=new HTMLEditorKit();
 	private HTMLDocument hd=(HTMLDocument) he.createDefaultDocument();
+	private StringBuilder html=new StringBuilder("");
 
 	public static final void main(String[] args)
 	{
@@ -20,28 +21,44 @@ public class ChattingBox extends JPanel
 		f.getContentPane().add(c);
 		f.pack();
 		f.setVisible(true);
-		c.pushMessage("");
-		c.pushMessage("");
-		c.pushMessage("");
+		c.pushMessage(true,"1");
+		c.pushMessage(true,"2");
+		c.pushMessage(false,"3");
+		c.pushMessage(false,"4");
+		c.pushMessage(false,"5");
+		c.pushMessage(false,"6");
+		c.pushMessage(true,"7");
+		c.pushMessage(false,"8");
+		c.pushMessage(true,"9");
+		System.out.println(c.myPane.getText());
 	}
 
 	ChattingBox()
 	{
-		myPane.setPreferredSize(new Dimension(100,200));
-		myPane.setEditorKit(he);
+		myPane.setPreferredSize(new Dimension(300,600));
+		//myPane.setEditorKit(he);
 		myPane.setContentType("text/html");
-		myPane.setDocument(hd);
+		//myPane.setDocument(hd);
 		myPane.setVisible(true);
 		this.add(myPane);
 	}
 
-	public void pushMessage(String message)
+	public void pushMessage(boolean ismyself,String message)
 	{
-		try{
-			he.insertHTML(hd,hd.getLength(),
-				"<table border=\"0\"><tr><td>000</td><td>123</td><td>456</td></tr></table>",
-				0,0,Tag.TABLE);
-			he.insertHTML(hd,hd.getLength(),"<br>",0,0,Tag.BR);
-		}catch(Exception e){}
+		html.append("<table border=\"0\" style=\"width:100%;\" cellspacing=\"0\">"+
+			"<tr align=\"center\"><td width=\"50px\">"+
+			((!ismyself)?"<img"+
+			" src=\""+this.getClass().getResource("")+"ask.jpg\">":"")+
+			"</td>"+
+			"<td style=\"white-space:nowrap;background-image:url("+this.getClass().getResource("")+"bg"+
+			(ismyself?"right":"left")+
+			".jpg);\">"+
+			message+
+			"</td>"+
+			"<td width=\"50px\">"+
+			(ismyself?"<img"+
+			" src=\""+this.getClass().getResource("")+"ask.jpg\">":"")+
+			"</td></tr></table><br>");
+		myPane.setText(html.toString());
 	}
 }
