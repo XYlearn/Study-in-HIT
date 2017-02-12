abf6c75307be2c90b73322ba92b75ddba11a79b8
kqwyf <kqwyfhit@163.com>
2017-02-12 21:19:03 +0800

    2017-2-12 创建NetBeans项目Client

--- Client/src/bin/test.java
+++ Client/src/bin/test.java
@@ -10,29 +10,29 @@ import util.AudioTools;
 import NetEvent.Client;
 import java.awt.Dimension;
 import java.awt.BorderLayout;
-import org.jsoup.Jsoup;
+import java.lang.Thread;
 
-public class test
-{
-	public static final String MAINPATH=test.class.getResource("").getPath()
-		.substring(0,test.class.getResource("").getPath().length()-4);
-	public static final String PICTPATH=MAINPATH+"pictures/";
-	public static final String FILEPATH=MAINPATH+"files/";
-	public static final Client client=new Client();
-	
-	public static final void main()
-	{
-		JFrame f=new JFrame("Hello, world!");
-		//ChattingBox c=new ChattingBox();
-		InputBox box=new InputBox();
-		box.setQuestionID("10000");
-		f.getContentPane().add(box,BorderLayout.CENTER);
-		f.setVisible(true);
-		//c.setSize(500,550);
-		box.setSize(500,300);
-		f.pack();
-		f.setLocationRelativeTo(null);
-		/*ArrayList<String> a=new ArrayList<String>();
+public class test {
+
+    public static final String MAINPATH = test.class.getResource("").getPath()
+            .substring(0, test.class.getResource("").getPath().length() - 4);
+    public static final String PICTPATH = MAINPATH + "pictures/";
+    public static final String FILEPATH = MAINPATH + "files/";
+    public static Client client = new Client();
+
+    public static final void main() {
+        new Thread(client).start();
+        JFrame f = new JFrame("Hello, world!");
+        //ChattingBox c=new ChattingBox();
+        InputBox box = new InputBox();
+        box.setQuestionID("10000");
+        f.getContentPane().add(box, BorderLayout.CENTER);
+        f.setVisible(true);
+        //c.setSize(500,550);
+        box.setSize(500, 300);
+        f.pack();
+        f.setLocationRelativeTo(null);
+        /*ArrayList<String> a=new ArrayList<String>();
 		a.add("ask.jpg");
 		c.pushMessage(new MyMessage("ask","22:01:25","what?"));
 		c.pushMessage(new MyMessage("a","22:01:25","this\n%0",a));
@@ -53,43 +53,36 @@ public class test
 			else
 				c.pushMessage(new MyMessage("a","22:02:24",cmd));
 		}*/
-		//c.pushMessage(new MyMessage("ask","22:11:15","en"));
-		//c.pushMessage(new MyMessage("a","22:11:15","en"));
-		//System.out.println(c.myPane.getText());
-		Scanner s=new Scanner(System.in);
-		String cmd,tmp=null;
-		while(s.hasNext())
-		{
-			cmd=s.nextLine();
-			if(cmd.equals("exit"))
-				return;
-			else if(cmd.equals("show"))
-			{
-				System.out.println(box.myPane.getText());
-				tmp=box.myPane.getText();
-			}
-			else if(cmd.equals("get"))
-				System.out.println(box.myPane.getCaretPosition());
-			else if(cmd.equals("send"))
-				box.sendMessage();
-			else if(cmd.equals("reset"))
-				tmp=box.myPane.getText();
-			else if(cmd.equals("insert"))
-				box.insertImage(PICTPATH+"ask.jpg");
-			else if(cmd.equals("launch"))
-				try
-				{
-					client.launchRequest("XYlearn","123456");
-				}
-				catch(Exception e)
-				{
-					System.out.println("登录失败："+e);
-				}
-			else
-			{
-				tmp=tmp.replaceAll(cmd,"");
-				System.out.println(tmp);
-			}
-		}
-	}
+        //c.pushMessage(new MyMessage("ask","22:11:15","en"));
+        //c.pushMessage(new MyMessage("a","22:11:15","en"));
+        //System.out.println(c.myPane.getText());
+        Scanner s = new Scanner(System.in);
+        String cmd, tmp = null;
+        while (s.hasNext()) {
+            cmd = s.nextLine();
+            if (cmd.equals("exit")) {
+                return;
+            } else if (cmd.equals("show")) {
+                System.out.println(box.myPane.getText());
+                tmp = box.myPane.getText();
+            } else if (cmd.equals("get")) {
+                System.out.println(box.myPane.getCaretPosition());
+            } else if (cmd.equals("send")) {
+                box.sendMessage();
+            } else if (cmd.equals("reset")) {
+                tmp = box.myPane.getText();
+            } else if (cmd.equals("insert")) {
+                box.insertImage(PICTPATH + "ask.jpg");
+            } else if (cmd.equals("launch")) {
+                try {
+                    client.launchRequest("XYlearn", "123456");
+                } catch (Exception e) {
+                    System.out.println("登录失败：" + e);
+                }
+            } else {
+                tmp = tmp.replaceAll(cmd, "");
+                System.out.println(tmp);
+            }
+        }
+    }
 }
